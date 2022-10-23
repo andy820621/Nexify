3268782898d932a73a3ea8f6fb9da8ed0f2ab3dc
BarZ <andy820621@gmail.com>
2022-10-23 13:30:38 +0900

    test

--- api/get.js
+++ api/get.js
@@ -4,6 +4,7 @@ export default async (req, res) => {
 			"http://nexifytw.mynetgear.com:45000/api/Record/GetRecords"
 		);
 		res.set("Access-Control-Allow-Origin", "*");
+		console.log(data);
 		res.send(data.data.Data);
 	} catch (err) {
 		res.send({ message: err });
