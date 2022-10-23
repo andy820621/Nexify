478bb8be4a843cf373ec9345ed1131f05c68869e
BarZ <andy820621@gmail.com>
2022-10-23 14:27:45 +0900

    test

--- api/get.js
+++ api/get.js
@@ -1,7 +1,6 @@
-import axios from "axios";
 export default async (req, res) => {
 	try {
-		let data = await axios.get(
+		let data = await fetch(
 			"http://nexifytw.mynetgear.com:45000/api/Record/GetRecords"
 		);
 		console.log(data);
