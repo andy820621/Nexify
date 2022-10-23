64b635175049691412678751a1d1e66abf71b4c9
BarZ <andy820621@gmail.com>
2022-10-23 14:16:28 +0900

    test

--- api/get.js
+++ api/get.js
@@ -1,8 +1,17 @@
-export default async (req, res) => {
+async function getData() {
 	try {
 		let data = await fetch(
 			"http://nexifytw.mynetgear.com:45000/api/Record/GetRecords"
 		);
+		return data;
+	} catch (err) {
+		return err;
+	}
+}
+
+export default async (req, res) => {
+	try {
+		let data = await getData();
 		console.log(data);
 		res.status(200).json({
 			body: data,
