1a3c64c12b270c33baaa000e1b9a7e018865e8f8
BarZ <andy820621@gmail.com>
2022-10-23 14:22:03 +0900

    test

--- api/get.js
+++ api/get.js
@@ -1,19 +1,9 @@
 import axios from "axios";
-
-async function getData() {
+export default async (req, res) => {
 	try {
 		let data = await axios.get(
 			"http://nexifytw.mynetgear.com:45000/api/Record/GetRecords"
 		);
-		return data;
-	} catch (err) {
-		return err;
-	}
-}
-
-export default async (req, res) => {
-	try {
-		let data = await getData();
 		console.log(data);
 		res.status(200).json({
 			body: data,
