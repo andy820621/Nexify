3298f918eebc687b9b9719e23049f37ec3fc55d3
BarZ <andy820621@gmail.com>
2022-10-23 13:56:38 +0900

    test

--- api/get.js
+++ api/get.js
@@ -1,11 +1,11 @@
 export default async (req, res) => {
 	try {
-		let data = await axios.get(
+		let data = await fetch(
 			"http://nexifytw.mynetgear.com:45000/api/Record/GetRecords"
 		);
 		res.set("Access-Control-Allow-Origin", "*");
 		console.log(data);
-		res.json(data);
+		res.send(data);
 	} catch (err) {
 		res.send({ message: err });
 	}
